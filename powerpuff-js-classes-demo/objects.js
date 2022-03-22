82a954813f987bee05cc3bdd4b1d209e50e67313
claire devillers <clairedevillers@hotmail.fr>
2022-03-22 17:58:08 -0400

    fix minor issues

--- powerpuff-js-classes-demo/objects.js
+++ powerpuff-js-classes-demo/objects.js
@@ -1,51 +1,66 @@
 
 class Enemy { // declaring a class
-    constructor(thickness, color) {
+    constructor(borderThickness=1, borderColor="red") {
         this.health = 100;
+        this.X = Math.floor(Math.random()*window.innerWidth);
+        this.Y = Math.floor(Math.random()*window.innerHeight);
+        this.dom_element = null;
+
+        this.draw(borderThickness, borderColor);
+        this.move();
+    }
+
+    draw = (borderThickness, borderColor) => {
+        let body = document.querySelector('body');
         let enemy = document.createElement('img');
         enemy.src="mojo.png";
         enemy.style.width="10%";
         enemy.id='mojo';
         enemy.style.position="absolute";
-        enemy.style.border=thickness + "px solid " + color;
+        enemy.style.border=borderThickness + "px solid " + borderColor;
         enemy.innerHTML="Mojo";
 
-        //will fail:
-        //enemy.addEventListener('click',this.clicked);
+
+        enemy.addEventListener('click', this.handleClicked);
 
          // will succeed
-        this.boundClicked = evt => this.clicked(evt);
-        enemy.addEventListener('click',this.boundClicked);
+        // this.boundClicked = evt => this.clicked(evt);
+        // enemy.addEventListener('click',this.boundClicked);
 
         this.dom_element = enemy;
-        this.lastX = window.innerWidth/2;
-        this.lastY = window.innerHeight/2;
-        let body = document.querySelector('body');
         body.appendChild(this.dom_element);
-        this.move();
     }
 
-    clicked() {
+    handleClicked =() => {
         this.health = this.health - 10;
         alert("Ahhh! You got me! My health is now " + this.health);
     }
 
-    move() {
+    move = () => {
         this.dom_element.style.left=this.get_new_X() + 'px';
         this.dom_element.style.top=this.get_new_Y() + 'px';
         console.log("move called");
+        setTimeout(this.move,500)
+    }
+
+    get_sign() {
+        if (Math.floor(Math.random() * 2) == 1) {
+            return +1
+        } else {
+            return -1;
+        }
     }
 
     get_new_X() {
-        let movement = (Math.random()*100)-50;
-        this.lastX = Math.min(Math.max(0,this.lastX + movement),window.innerWidth);
-        return this.lastX;
+        let movement = (Math.random()*200)-50;
+        this.X = this.X + this.get_sign()*movement;
+        return this.X;
     }
 
     get_new_Y() {
-        let movement = (Math.random()*100)-50;
-        this.lastY = Math.min(Math.max(0,this.lastY + movement),window.innerHeight);
-        return this.lastY;
+        let movement = (Math.random()*200)-50;
+        this.Y = this.Y + this.get_sign()*movement;
+        return this.Y;
     }
 }
 
@@ -56,13 +71,13 @@ class HealthyEnemy extends Enemy { // inheritance
     }
 }
 
+// we could put all that is below into something like class Game {}
+// but skipping for simplicity
+
 let enemy1 = new Enemy("1","green"); // instantiate a class
 let enemy2 = new HealthyEnemy() // instantiating a child
-
-function render() {
-    enemy1.move();
-    enemy2.move();
-    setTimeout(render,500);
+let enemies = []
+for (let i = 0; i < 10; i++) {
+    let randomEnemy = new Enemy(Math.floor(Math.random()*100));
+    enemies.push(randomEnemy);
 }
-
-render();
